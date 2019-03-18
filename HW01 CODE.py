47d38ea7978f6d0be2ab6689b68307705dbf8287
Kagostino <47874905+Kagostino@users.noreply.github.com>
2019-03-18 13:06:08 +0800

    Add files via upload

---
+++ HW01 CODE.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+A=[]
+with open(r'C:\Users\User\Desktop\HW01 all.csv', newline='') as csvfile:
+    rows = csv.DictReader(csvfile)
+    B=[]
+    for row in rows:
+        B.append(row['Symbol'])
+for x in B:
+    try:
+        file='C:\\Users\\User\\Desktop\\ETF\\'+x+'.csv'
+        data = pd.read_csv(file,encoding='utf-8-sig')
+        data
+        s1=pd.Series(data['Date'])
+        s2=pd.Series(data['Close'])
+        df = pd.DataFrame({'Date':s1,'close':s2})
+        A.append(df)
+    except:
+        pass
+#_PRN額外處理
+file='C:\\Users\\User\\Desktop\\ETF\\_PRN.csv'
+data = pd.read_csv(file,encoding='utf-8-sig')
+s1=pd.Series(data['Date'])
+s2=pd.Series(data['Close'])
+df = pd.DataFrame({'Date':s1,'close':s2})
+A.append(df)
